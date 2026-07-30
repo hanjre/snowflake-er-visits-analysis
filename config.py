19d93b6324c656ede1119c2c40e48e8c3483d6e9
hanjre <20497967+hanjre@users.noreply.github.com>
2026-07-30 00:20:57 -0500

    Add MFA support to Snowflake connection

--- config.py
+++ config.py
@@ -2,14 +2,15 @@ import os
 import getpass
 import snowflake.connector
 
-
 def get_connection():
     password = getpass.getpass("Snowflake password: ")
+    passcode = getpass.getpass("Authenticator code: ")
 
     return snowflake.connector.connect(
         account=os.getenv("SNOWFLAKE_ACCOUNT", "UZSDACD-JZ99935"),
         user=os.getenv("SNOWFLAKE_USER", "HANJRE"),
         password=password,
+        passcode=passcode,
         warehouse=os.getenv("SNOWFLAKE_WAREHOUSE", "COMPUTE_WH"),
         database=os.getenv("SNOWFLAKE_DATABASE", "PORTFOLIO_DB"),
         schema=os.getenv("SNOWFLAKE_SCHEMA", "ANALYTICS"),
